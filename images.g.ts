22ac64bd2592b3c66ffe098027be60fe7700db19
Cuhwaii <59039810+shafay39@users.noreply.github.com>
2023-03-01 09:42:29 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,62 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myEnemy":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f e e 2 2 2 2 2 2 e f f . . 
+. f f e 2 f f f f f f 2 e f f . 
+. f f f f f e e e e f f f f f . 
+. . f e f b f 4 4 f b f e f . . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . e f e 4 d d d d 4 e f . . . 
+. . e 4 d d e 2 2 2 2 f e f . . 
+. . . e d d e 2 2 2 2 f 4 e . . 
+. . . . e e f 5 5 4 4 f . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . . . . f f f . . . . 
+`;
+            case "image2":
+            case "mysincity":return img`
+. . . . 
+. . . . 
+. . . . 
+. . . . 
+`;
+            case "image3":
+            case "bob":return img`
+. . . . . . . . 
+. . . . . . 3 . 
+. . . . . . f f 
+. . . . . f f 5 
+. f f f f f f f 
+f f f f f f . . 
+. f . . f . . . 
+. f . . f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
